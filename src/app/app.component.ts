75e33a99c27700b343e58b3ff340d656033b1f36
Francary <francarymolinapaez@gmail.com>
2022-10-15 16:01:02 -0300

    Repasando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name = 'Angular ' + VERSION.major;
+  name = 'Angular Recalculando' + VERSION.major;
+}
+export class Micro {
+
+  primer = ' Micro Linea A';
+
+}
+export class DobleM {
+
+  primer = ' Dos Pisos';
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+
 import { HelloComponent } from './hello.component';
+import { AppComponent2Component } from './components/app-component2/app-component2.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule, ],
+  declarations: [ AppComponent, HelloComponent, AppComponent2Component],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
